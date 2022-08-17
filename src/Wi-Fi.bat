053fb5c04c9d442abf2eea8cd4cffe421b8e7a63
Nazar Ansari <nazaransari06@gmail.com>
2022-08-17 16:40:17 +0530

    Added Banner Starting of the program

---
+++ src/Wi-Fi.bat
@@ -0,0 +1,26 @@
+::                      Turning Off The Environment Prompt
+@ECHO OFF
+
+::                      Enabling The Variable Expansion Throughout The Script
+SETLOCAL EnableDelayedExpansion
+COLOR 0a      
+
+::                      Assigning The Title to the Program
+TITLE Wi-Fi Friendly Tool     
+CLS       
+
+::                      Creating Banner For User
+ECHO.
+ECHO.        
+ECHO        ###       ### ########## ###         ########   ########  ####    ####  ########## 
+ECHO        ###       ### ###        ###        ###    ### ###    ### ###### ###### ###        
+ECHO        ###       ### ###        ###        ###        ###    ### ### ##### ### ###        
+ECHO        ###  ###  ### ########   ###        ###        ###    ### ###  ###  ### ########   
+ECHO        ### ##### ### ###        ###        ###        ###    ### ###       ### ###        
+ECHO         ##### #####  ###        ###        ###    ### ###    ### ###       ### ###        
+ECHO          ###   ###   ########## ##########  ########   ########  ###       ### ########## 
+ECHO.
+ECHO.
+ECHO.
+ECHO    Press " E.N.T.E.R " to Start The Application . . . . . . 
+PAUSE > NULL
