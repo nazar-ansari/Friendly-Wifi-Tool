555cd9d20f6296d3c07ae78bc89adf9942084b03
Nazar Ansari <nazaransari06@gmail.com>
2022-08-17 16:41:24 +0530

    process for checking Administrative priviledges

--- src/Wi-Fi.bat
+++ src/Wi-Fi.bat
@@ -24,3 +24,49 @@ ECHO.
 ECHO.
 ECHO    Press " E.N.T.E.R " to Start The Application . . . . . . 
 PAUSE > NULL
+
+::                          Process To Check Administrative Priviledges
+COLOR 0b
+CLS
+ECHO.
+ECHO.
+ECHO                Checking For The Administrative Priviledges . . . . . . . . . .
+ping localhost -n 2 >NULL
+CLS
+ECHO.
+ECHO.
+ECHO                Checking For The Administrative Priviledges . . .  
+ping localhost -n 2 >NULL
+CLS
+ECHO.
+ECHO.
+ECHO                Checking For The Administrative Priviledges . . . . . . . . . .
+ping localhost -n 2 >NULL
+CLS
+ECHO.
+ECHO.
+ECHO                Checking For The Administrative Priviledges . . .  
+ping localhost -n 2 >NULL
+CLS
+net session >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+COLOR 0a
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO                            *-----------------------------------------------*
+ECHO                                Granted The Administrative Priviledges 
+ECHO                            *-----------------------------------------------*
+ECHO.
+ping localhost -n 3 >null
+) ELSE (
+COLOR 7C
+ECHO.
+ECHO        [  Please Run This Program As an Administrative  ]
+ECHO.
+ping localhost -n 1 >NULL
+IF EXIST "NULL" DEL NULL
+PAUSE >NULL
+GOTO :ProgramExit
+)
